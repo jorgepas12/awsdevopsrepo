de86c314335852e3aa8cff5ed036be22c4eb2592
jorgepas12 <57460491+jorgepas12@users.noreply.github.com>
2025-05-21 21:55:35 -0600

    Update start.sh

--- app/scripts/start.sh
+++ app/scripts/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Iniciando contenedor static-web..."
 cd /home/ec2-user/app
-docker build -t static-web .
-docker run -d --name static-web -p 80:80 static-web
+docker pull 316121670951.dkr.ecr.us-east-1.amazonaws.com/static-web:v1
+docker run -d --name static-web -p 80:80 316121670951.dkr.ecr.us-east-1.amazonaws.com/static-web:v1
