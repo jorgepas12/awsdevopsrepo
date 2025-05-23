d8b97d37202136076357b381e0664447113826f9
Jorge Pasaron <jorge.pasaron@netec.com.mx>
2025-05-22 19:13:07 -0600

    ajuste de implementacion

---
+++ app/deploy-ec2/scripts/start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Iniciando contenedor static-web..."
+cd /home/ec2-user/app
+docker pull 316121670951.dkr.ecr.us-east-1.amazonaws.com/static-web:sweb-v1
+docker run -d --name static-web -p 80:80 316121670951.dkr.ecr.us-east-1.amazonaws.com/static-web:sweb-v1
--- app/scripts/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Iniciando contenedor static-web..."
-cd /home/ec2-user/app
-docker pull 316121670951.dkr.ecr.us-east-1.amazonaws.com/static-web:v1
-docker run -d --name static-web -p 80:80 316121670951.dkr.ecr.us-east-1.amazonaws.com/static-web:v1
